c8c2d5c9072d92a151df6881428996eacaa68e01
rashmi chouhan <rashmichouhan441@gmail.com>
2023-08-24 19:20:14 +0530

    intitial

---
+++ data/database.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export const connectDB = () =>
+  mongoose
+    .connect(process.env.MONGO_URL, { dbName: "backendapi" })
+    .then(() => console.log("Database Connected"))
+    .catch((e) => console.log(e));
---
+++ models/task.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const Task = mongoose.model("Task", schema);
